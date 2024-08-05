1ec50d8feb05ad6135cf91f4de2111e59f294f9c
Tokens-buy-Time <Tokens.buy.Time@gmail.com>
2024-08-05 00:50:02 -0400

    Update claeor.py
    Inverted commas syntax error
    
    Version 2.6.3

--- claeor.py
+++ claeor.py
@@ -162,7 +162,7 @@ def start_claeor_tool():
             for year, data in operational_data.items():
                 financials = calculate_financials(year, assumptions, data)
                 all_financials[year] = financials 
-                total_capital_supplied += financials[“capital_supplied”]
+                total_capital_supplied += financials["capital_supplied"]
 
         df_financials = pd.DataFrame(all_financials).T
 
