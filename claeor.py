1854fc2d4f5a8f657026bf259f95349e5262fca7
Tokens-buy-Time <Tokens.buy.Time@gmail.com>
2024-08-05 10:07:09 -0400

    Update claeor.py
    Troubleshooting input errors
    
    Version  3.01

--- claeor.py
+++ claeor.py
@@ -188,9 +188,5 @@ def start_claeor_tool():
         st.write(f"Return on Investment (ROI) over 10 years: {roi:.2f}%")
     else:
         st.warning("Please input assumptions and operational data first.")
-
-elif
-    
-if __name__ == "__main__":
-     start_claeor_tool()
+        start_claeor_tool()
     
