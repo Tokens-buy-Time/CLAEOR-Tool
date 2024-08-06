9fde3c4828746e9b85a42cd9d2dbc159bd186774
Tokens-buy-Time <Tokens.buy.Time@gmail.com>
2024-08-05 20:32:24 -0400

    Update claeor.py
    Version B1.0.10

--- claeor.py
+++ claeor.py
@@ -66,7 +66,7 @@ def operations_data_screen():
 
            if st.button(f"Save Year {year_data[i,1]} Data"):
                st.success(f"Year {year_data[i,1]} data saved!")
-        i=i+1
+           i=i+1
     elif
 
 # Function to display financial statements
