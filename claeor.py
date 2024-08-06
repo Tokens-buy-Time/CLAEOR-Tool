356273d5b89c74c5e57f7c7b554c8b05b53c9100
Tokens-buy-Time <Tokens.buy.Time@gmail.com>
2024-08-06 16:17:31 -0400

    Update claeor.py
    Test

--- claeor.py
+++ claeor.py
@@ -79,6 +79,7 @@ def operations_data_screen():
        if st.button(f"Save year's input data"):
           st.success(f"Year's input data saved!")
     i=i+1
+    elif
 
 # Function to display financial statements
 def financial_statements_screen():
