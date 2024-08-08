5c1059941b5d20f621334625c04b7f77f3cf044e
Tokens-buy-Time <Tokens.buy.Time@gmail.com>
2024-08-08 10:32:27 -0400

    Update claeor.py
    Test

--- claeor.py
+++ claeor.py
@@ -223,8 +223,7 @@ def operations_screen_1():
         st.session_state["operations_data_1"][i] = st.number_input(label="Operating Expenses", value=st.session_state["operations_data_1"][i])
         i=i+1
         st.session_state["operations_data_1"][i] = st.number_input(label="Investor Capital Calls $", value=st.session_state["operations_data_1"][i])
-        i=100 # end loop
-        
+        write(i)
     if st.button(f"Save year's input data"):
         st.success(f"Year's input data saved!")
             
