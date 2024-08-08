2c23438d1cf24c53193810ca266fddd9a9b33e3c
Tokens-buy-Time <Tokens.buy.Time@gmail.com>
2024-08-07 20:12:13 -0400

    Update claeor.py
    Test

--- claeor.py
+++ claeor.py
@@ -680,7 +680,7 @@ elif menu == "Assumptions":
 elif menu == "Year 1 Operations":
     rerun_1=0
 # operations_screen_1(rerun_1)
-    if rerun_1=1
+    if rerun_1==1
        rerun_1=0
 # operation_screen_1(rerun_1)
     elif
@@ -688,7 +688,7 @@ elif menu == "Year 1 Operations":
 elif menu == "Year 2 Operations":
     rerun_2=0
 # operations_screen_2(rerun_2)
-    if rerun_2=1
+    if rerun_2==1
        rerun_2=0
 # operation_screen_2(rerun_2)
     elif
@@ -696,7 +696,7 @@ elif menu == "Year 2 Operations":
 elif menu == "Year 3 Operations":
     rerun_3=0
 # operations_screen_3(rerun_3)
-    if rerun_3=1
+    if rerun_3==1
        rerun_3=0
 # operation_screen_3(rerun_3)
     elif
@@ -704,7 +704,7 @@ elif menu == "Year 3 Operations":
 elif menu == "Year 4 Operations":
     rerun_4=0
 # operations_screen_4(rerun_4)
-    if rerun_4=1
+    if rerun_4==1
        rerun_4=0
 # operation_screen_4(rerun_4)
     elif
@@ -712,7 +712,7 @@ elif menu == "Year 4 Operations":
 elif menu == "Year 5 Operations":
     rerun_5=0
 # operations_screen_5(rerun_5)
-    if rerun_5=1
+    if rerun_5==1
        rerun_5=0
 # operation_screen_5(rerun_5)
     elif
@@ -720,7 +720,7 @@ elif menu == "Year 5 Operations":
 elif menu == "Year 6 Operations":
     rerun_6=0
 # operations_screen_6(rerun_6)
-    if rerun_6=1
+    if rerun_6==1
        rerun_6=0
 # operation_screen_6(rerun_6)
     elif
@@ -728,7 +728,7 @@ elif menu == "Year 6 Operations":
 elif menu == "Year 7 Operations":
     rerun_7=0
 # operations_screen_7(rerun_7)
-    if rerun_7=1
+    if rerun_7==1
        rerun_7=0
 # operation_screen_7(rerun_7)
     elif
@@ -736,7 +736,7 @@ elif menu == "Year 7 Operations":
 elif menu == "Year 8 Operations":
     rerun_8=0
 # operations_screen_8(rerun_8)
-    if rerun_8=1
+    if rerun_8==1
        rerun_8=0
 # operation_screen_8(rerun_8)
     elif
@@ -744,7 +744,7 @@ elif menu == "Year 8 Operations":
 elif menu == "Year 9 Operations":
     rerun_9=0
 # operations_screen_9(rerun_9)
-    if rerun_9=1
+    if rerun_9==1
        rerun_9=0
 # operation_screen_9(rerun_9)
     elif
@@ -752,7 +752,7 @@ elif menu == "Year 9 Operations":
 elif menu == "Year 10 Operations":
     rerun_10=0
 # operations_screen_10(rerun_10)
-    if rerun_10=1
+    if rerun_10==1
        rerun_10=0
 # operation_screen_10(rerun_10)
     elif
