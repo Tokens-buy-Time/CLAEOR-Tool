7ef41115a09ddbfc6a9373173da2e2b9b7297792
Tokens-buy-Time <Tokens.buy.Time@gmail.com>
2024-08-08 14:55:15 -0400

    Update claeor.py
    Test

--- claeor.py
+++ claeor.py
@@ -675,7 +675,8 @@ def operations_screen_10():
 
 # Function to display financial statements
 def financial_statements_screen():
-    
+    st.header("Financial Statements")    
+    st.write(" ")
     st.write("Each year's Income, Balance and Cash Flow Statement use the last saved data for the particular year")
     Income_Year()
     Balance_Sheet_Year()
@@ -684,28 +685,24 @@ def financial_statements_screen():
 
 # Function to display Income Statement
 def Income_Year():
-    st.header("Financial Statements")
     st.subheader("Income Statement")
     st.write("Income statement details will be displayed here.")
 
 
 # Function to display Balance Sheet Statement
 def Balance_Sheet_Year():
-    st.header("Financial Statements")
     st.subheader("Balance Sheet")
     st.write("Balance sheet details will be displayed here.")
 
 
 # Function to display Cash Flow Statement
 def Cash_Flow_Year(): 
-    st.header("Financial Statements")   
     st.subheader("Cash Flow Statement")
     st.write("Cash flow statement details will be displayed here.")
 
 
 # Function to display performance metrics
 def performance_metrics_screen():
-    st.header("Financial Statements")      
     st.header("Performance Metrics")
     st.write("Metrics will be calculated and displayed here based on the input data.")
 
