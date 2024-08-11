d08269a33f9729afb817e2a55fa46faf0ba8669c
Tokens-buy-Time <Tokens.buy.Time@gmail.com>
2024-08-11 01:39:45 -0400

    Update claeor.py
    Test

--- claeor.py
+++ claeor.py
@@ -815,9 +815,10 @@ def performance_metrics_screen(assumptions, operations_data_1, operations_data_2
     st.write("Metrics calculated and displayed here are based upon all of the input data.")
     st.write(" ")
 
-    for i in range(1,10):
-        calculate_financials(Year_n, assumptions, operations_data)                          
-        Year_n += 1
+    for i in range(0,9):
+         Year_n += 1
+         calculate_financials(Year_n, assumptions, operations_data)                          
+       
 
 # Compile "all_financial" data from Financial Statements 
     
